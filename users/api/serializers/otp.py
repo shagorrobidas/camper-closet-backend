ac66b69b8e6817ed46118068748c4a3bcb5481e1
shagorrobidas <roysagor88@gmail.com>
2026-02-25 04:53:19 +0600

    feat otp serializers add

---
+++ users/api/serializers/otp.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+
+class OTPSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    purpose = serializers.ChoiceField(choices=[
+        'email_verification',
+        'password_reset',
+        'change_email'
+    ])
+
+
+class OTPVerificationSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    otp = serializers.CharField(max_length=6)
+    purpose = serializers.ChoiceField(choices=[
+        'email_verification',
+        'password_reset',
+        'change_email'
+    ])
