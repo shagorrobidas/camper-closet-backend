a698aabf3dc0dacfd58e5ac087d563eea233ac21
shagorrobidas <roysagor88@gmail.com>
2026-02-25 04:31:23 +0600

    feat: signup url

---
+++ users/api/urls.py
@@ -0,0 +1,110 @@
+from django.urls import path
+from .views import (
+    RegisterView,
+    # LoginView,
+    # RequestOTPView,
+    # VerifyOTPView,
+    # ChangePasswordView,
+    # RequestPasswordResetView,
+    # VerifyPasswordResetOTPView,
+    # SetNewPasswordView,
+    # CheckPasswordResetStatusView,
+    # LogoutView,
+    # UserProfileView,
+    # UserProfileUpdateView,
+    # UserProfileDeleteView,
+    # RequestEmailChangeView,
+    # VerifyEmailChangeView,
+    # GoogleSocialAuthView,
+    # AppleSocialAuthView
+)
+
+urlpatterns = [
+    path(
+        'signup/',
+        RegisterView.as_view(),
+        name='signup'
+    ),
+    # path(
+    #     'login/',
+    #     LoginView.as_view(),
+    #     name='login'
+    # ),
+    # path(
+    #     'request-otp/',
+    #     RequestOTPView.as_view(),
+    #     name='request_otp'
+    # ),
+    # path(
+    #     'verify-otp/',
+    #     VerifyOTPView.as_view(),
+    #     name='verify_otp'
+    # ),
+    # path(
+    #     'change-password/',
+    #     ChangePasswordView.as_view(),
+    #     name='change_password'
+    # ),
+    # path(
+    #     'request-password-reset/',
+    #     RequestPasswordResetView.as_view(),
+    #     name='request_password-reset'
+    # ),
+    # path(
+    #     'verify-password-reset-otp/',
+    #     VerifyPasswordResetOTPView.as_view(),
+    #     name='verify_password_reset_otp'
+    # ),
+    # path(
+    #     'set-new-password/',
+    #     SetNewPasswordView.as_view(),
+    #     name='set_new_password'
+    # ),
+    # path(
+    #     'check-reset-status/',
+    #     CheckPasswordResetStatusView.as_view(),
+    #     name='check_reset_status'
+    # ),
+
+    # path(
+    #     'profile/',
+    #     UserProfileView.as_view(),
+    #     name='profile'
+    # ),
+    # path(
+    #     'profile/update/',
+    #     UserProfileUpdateView.as_view(),
+    #     name='profile_update'
+    # ),
+    # path(
+    #     'profile/delete/',
+    #     UserProfileDeleteView.as_view(),
+    #     name='profile_delete'
+    # ),
+    # path(
+    #     'logout/',
+    #     LogoutView.as_view(),
+    #     name='logout'
+    # ),
+    # path(
+    #     'change-email/request/',
+    #     RequestEmailChangeView.as_view(),
+    #     name='request_email_change'
+    # ),
+    # path(
+    #     'change-email/verify/',
+    #     VerifyEmailChangeView.as_view(),
+    #     name='verify_email_change'
+    # ),
+    # path(
+    #     'social-auth/google/',
+    #     GoogleSocialAuthView.as_view(),
+    #     name='google_social_auth'
+    # ),
+    # path(
+    #     'social-auth/apple/',
+    #     AppleSocialAuthView.as_view(),
+    #     name='apple_social_auth'
+    # ),
+
+]
