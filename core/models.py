a34c5d677b2ddc453555dfe9e3e9926f50310efa
shagorrobidas <roysagor88@gmail.com>
2026-02-28 06:05:20 +0600

    refactor: add field id, deleted_at and soft_delete function

--- core/models.py
+++ core/models.py
@@ -1,13 +1,27 @@
 from django.db import models
+import uuid
+from django.utils import timezone
 
 
 class BaseModel(models.Model):
     """
     Abstract base model with common fields
     """
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
 
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
     class Meta:
         abstract = True
+
+    def soft_delete(self):
+        self.deleted_at = timezone.now()
+        self.save(update_fields=["deleted_at"])
