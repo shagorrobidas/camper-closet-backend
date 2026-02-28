79d527ca95ce038fb5e14f3d4461d7b56d9b053a
shagorrobidas <roysagor88@gmail.com>
2026-02-28 23:37:49 +0600

    feat: Verify Password ResetOTP Serializer

--- users/api/serializers/reset_password.py
+++ users/api/serializers/reset_password.py
@@ -3,3 +3,8 @@ from rest_framework import serializers
 
 class ResetPasswordSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
+
+
+class VerifyPasswordResetOTPSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    otp = serializers.CharField(max_length=6, required=True)
