a3cfcf76e3562bc4b9f193b26789df15ed6d46bf
shagorrobidas <roysagor88@gmail.com>
2026-03-03 04:15:34 +0600

    init file

--- closet/api/serializers/__init__.py
+++ closet/api/serializers/__init__.py
@@ -1,6 +1,10 @@
 from .item_catagory import ItemCategorySerializer
+from .brand_catagory import BrandCategoryTypeSerializer
+from .item import ClosetItemSerializer
 
 
 __all__ = [
     'ItemCategorySerializer',
+    'BrandCategoryTypeSerializer',
+    'ClosetItemSerializer',
 ]
--- closet/api/views/__init__.py
+++ closet/api/views/__init__.py
@@ -1,5 +1,20 @@
-from .item_catagory import ItemCategoryListView
+from .item_catagory import (
+    ItemCategoryListView,
+    ItemCategoryCreateView,
+)
+from .brand_catagory import (
+    BrandCategoryTypeListView,
+)
+from .item import (
+    ClosetItemListView,
+    ClosetItemCreateView,
+)
+
 
 __all__ = [
     'ItemCategoryListView',
+    'ItemCategoryCreateView',
+    'BrandCategoryTypeListView',
+    'ClosetItemListView',
+    'ClosetItemCreateView',
 ]
