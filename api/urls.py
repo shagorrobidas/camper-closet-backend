0b5420929a06681cc7f303dc524342dab0401c19
shagorrobidas <roysagor88@gmail.com>
2026-03-03 05:28:19 +0600

    feat: dashboadr api url connect

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('user/', include('users.api.urls')),
     path('closet/', include('closet.api.urls')),
+    path('dashboard/', include('dashboard.api.urls')),
 ]
