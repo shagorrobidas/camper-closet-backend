46af04ebe2b1e0f7891034ad47c1a816e2bb56f3
shagorrobidas <roysagor88@gmail.com>
2026-03-03 04:15:13 +0600

    add: item create url

--- closet/api/urls.py
+++ closet/api/urls.py
@@ -1,10 +1,36 @@
 from django.urls import path
-from closet.api.views import ItemCategoryListView
+from closet.api.views import (
+    BrandCategoryTypeListView,
+    ItemCategoryListView,
+    ItemCategoryCreateView,
+    ClosetItemListView,
+    ClosetItemCreateView,
+)
 
 urlpatterns = [
     path(
+        'brand-category-types/',
+        BrandCategoryTypeListView.as_view(),
+        name='brand_category_types'
+    ),
+    path(
         'item-categories/',
         ItemCategoryListView.as_view(),
-        name='item-categories'
+        name='item_categories'
+    ),
+    path(
+        'item-categories/create/',
+        ItemCategoryCreateView.as_view(),
+        name='item_categories_create'
+    ),
+    path(
+        'items/',
+        ClosetItemListView.as_view(),
+        name='closet_items'
+    ),
+    path(
+        'items/create/',
+        ClosetItemCreateView.as_view(),
+        name='closet_items_create'
     ),
 ]
