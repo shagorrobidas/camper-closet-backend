b4e1cf7641bad7256b880f129c43018980d957bc
shagorrobidas <roysagor88@gmail.com>
2026-03-03 04:12:19 +0600

    feat:Item Category Create view

--- closet/api/views/item_catagory.py
+++ closet/api/views/item_catagory.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from closet.models import ItemCategory
 from closet.api.serializers import ItemCategorySerializer
 from users.permission import ProfileAccessMixin
@@ -13,8 +13,24 @@ class ItemCategoryListView(ProfileAccessMixin, ListAPIView):
         user = self.get_object()
         queryset = self.queryset.filter(user=user)
         serializer = self.get_serializer(queryset, many=True)
-        return CustomResponse(
+        return CustomResponse.success(
             data=serializer.data,
             message="Item categories retrieved successfully",
             status_code=200
+        )
+
+
+class ItemCategoryCreateView(ProfileAccessMixin, CreateAPIView):
+    queryset = ItemCategory.objects.all()
+    serializer_class = ItemCategorySerializer
+
+    def create(self, request, *args, **kwargs):
+        user = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=user)
+        return CustomResponse.success(
+            data=serializer.data,
+            message="Item category created successfully",
+            status_code=201
         )
