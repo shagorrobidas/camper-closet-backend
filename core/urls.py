bacd1acc6ca44ace1c45bb8301ee635a0dcabef5
shagorrobidas <roysagor88@gmail.com>
2026-03-04 06:01:00 +0600

    show home screen message

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import home
 
 urlpatterns = [
+    path(
+        '',
+        home
+    ),
     path(
         'admin/',
         admin.site.urls
