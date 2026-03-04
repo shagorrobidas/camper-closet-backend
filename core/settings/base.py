6ca96ee13cd5c2001b2f4b20e58402babb192e35
shagorrobidas <roysagor88@gmail.com>
2026-03-05 03:21:09 +0600

    fix: DEFAULT AUTO FIELD problem fix

--- core/settings/base.py
+++ core/settings/base.py
@@ -101,6 +101,12 @@ USE_I18N = True
 USE_TZ = True
 
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
