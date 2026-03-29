7e4049f1701c87157b3895cb7b8dcff305f83eb1
shagorrobidas <roysagor88@gmail.com>
2026-03-30 04:29:01 +0600

    fix: add permission class

--- closet/api/views/brand_catagory.py
+++ closet/api/views/brand_catagory.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 from closet.models import ItemCategoryType
 from closet.api.serializers import ItemCategoryTypeSerializer
 from core.utils import CustomResponse
@@ -7,6 +8,7 @@ from core.utils import CustomResponse
 class ItemCategoryTypeListView(ListAPIView):
     queryset = ItemCategoryType.objects.all()
     serializer_class = ItemCategoryTypeSerializer
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         queryset = self.queryset.all()
