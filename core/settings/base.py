0505a505c12f4597e9fa5fa0f005fd03b655c540
shagorrobidas <roysagor88@gmail.com>
2026-04-12 02:23:55 +0600

    update: access token expaire 1 min for testing

--- core/settings/base.py
+++ core/settings/base.py
@@ -186,7 +186,7 @@ LOGGING = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
