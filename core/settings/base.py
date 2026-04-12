231586e2492c3890a1f2bd33675e96a8c905e55d
shagorrobidas <roysagor88@gmail.com>
2026-04-12 06:00:17 +0600

    fix: token 10 days

--- core/settings/base.py
+++ core/settings/base.py
@@ -186,7 +186,7 @@ LOGGING = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
