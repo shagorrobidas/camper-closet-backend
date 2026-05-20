33829aed61ecaac0117b75ba27cd388be3530b30
Shagor Robidas <roysagor88@gmail.com>
2026-05-20 23:19:03 +0600

    admin: Refactor dashboard admin models to use Unfold ModelAdmin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from dashboard.models import BrandCategory, ShopWebsite
 
 
 @admin.register(BrandCategory)
-class BrandCategoryAdmin(admin.ModelAdmin):
+class BrandCategoryAdmin(ModelAdmin):
     list_display = ('id', 'name')
     search_fields = ('name',)
     ordering = ('name',)
 
 
 @admin.register(ShopWebsite)
-class ShopWebsiteAdmin(admin.ModelAdmin):
+class ShopWebsiteAdmin(ModelAdmin):
     list_display = ('id', 'name', 'website_url', 'is_active')
     list_filter = ('is_active', 'categories')
     search_fields = ('name', 'description')
