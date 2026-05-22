8ab893f24ab3a1b22f54f1e01e42eedf9a2270a6
Shagor Robidas <roysagor88@gmail.com>
2026-05-23 03:12:45 +0600

    style(admin): hide token blocklist and periodic tasks in admin panel

--- users/admin.py
+++ users/admin.py
@@ -27,36 +27,59 @@ class UserAdmin(ModelAdmin):
     readonly_fields = ('created_at', 'updated_at', 'deleted_at')
 
 
-@admin.register(Notification)
-class NotificationAdmin(ModelAdmin):
-    list_display = (
-        'id', 'user', 'type', 'title', 'is_read', 'created_at'
-    )
-    list_filter = ('type', 'is_read', 'created_at')
-    search_fields = ('user__email', 'title', 'body')
-    ordering = ('-created_at',)
-    readonly_fields = ('created_at', 'updated_at', 'deleted_at')
+# @admin.register(Notification)
+# class NotificationAdmin(ModelAdmin):
+#     list_display = (
+#         'id', 'user', 'type', 'title', 'is_read', 'created_at'
+#     )
+#     list_filter = ('type', 'is_read', 'created_at')
+#     search_fields = ('user__email', 'title', 'body')
+#     ordering = ('-created_at',)
+#     readonly_fields = ('created_at', 'updated_at', 'deleted_at')
 
 
-@admin.register(NotificationSetting)
-class NotificationSettingAdmin(ModelAdmin):
-    list_display = (
-        'id', 'user', 'enabled', 'packing_reminders',
-        'milestone_achievements', 'weekly_summaries'
-    )
-    list_filter = (
-        'enabled', 'packing_reminders',
-        'milestone_achievements', 'weekly_summaries'
-    )
-    search_fields = ('user__email',)
-    readonly_fields = ('created_at', 'updated_at', 'deleted_at')
+# @admin.register(NotificationSetting)
+# class NotificationSettingAdmin(ModelAdmin):
+#     list_display = (
+#         'id', 'user', 'enabled', 'packing_reminders',
+#         'milestone_achievements', 'weekly_summaries'
+#     )
+#     list_filter = (
+#         'enabled', 'packing_reminders',
+#         'milestone_achievements', 'weekly_summaries'
+#     )
+#     search_fields = ('user__email',)
+#     readonly_fields = ('created_at', 'updated_at', 'deleted_at')
 
 
-@admin.register(UserSubscriptionHistory)
-class UserSubscriptionHistoryAdmin(ModelAdmin):
-    list_display = (
-        'id', 'user', 'product_id', 'subscription_status', 'status', 'start_time', 'expiry_time'
-    )
-    list_filter = ('subscription_status', 'status', 'start_time', 'expiry_time')
-    search_fields = ('user__email', 'product_id', 'order_id')
-    readonly_fields = ('created_at', 'updated_at', 'deleted_at')
+# @admin.register(UserSubscriptionHistory)
+# class UserSubscriptionHistoryAdmin(ModelAdmin):
+#     list_display = (
+#         'id', 'user', 'product_id', 'subscription_status', 'status', 'start_time', 'expiry_time'
+#     )
+#     list_filter = ('subscription_status', 'status', 'start_time', 'expiry_time')
+#     search_fields = ('user__email', 'product_id', 'order_id')
+#     readonly_fields = ('created_at', 'updated_at', 'deleted_at')
+
+
+# Unregister simplejwt token blacklist models
+try:
+    from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
+    for model in [OutstandingToken, BlacklistedToken]:
+        try:
+            admin.site.unregister(model)
+        except admin.sites.NotRegistered:
+            pass
+except ImportError:
+    pass
+
+# Unregister django_celery_beat models
+try:
+    from django_celery_beat.models import PeriodicTask, IntervalSchedule, CrontabSchedule, SolarSchedule, ClockedSchedule, PeriodicTasks
+    for model in [PeriodicTask, IntervalSchedule, CrontabSchedule, SolarSchedule, ClockedSchedule, PeriodicTasks]:
+        try:
+            admin.site.unregister(model)
+        except admin.sites.NotRegistered:
+            pass
+except ImportError:
+    pass
