2b01ab1e57ea00767a1b09a9f1266dd8bfac83cb
Shagor Robidas <roysagor88@gmail.com>
2026-05-24 01:26:07 +0600

    refactor(closet): update serializer exports in init module

--- closet/api/serializers/__init__.py
+++ closet/api/serializers/__init__.py
@@ -2,9 +2,17 @@ from .item_catagory import ItemCategorySerializer
 from .brand_catagory import ItemCategoryTypeSerializer
 from .item import ClosetItemSerializer
 
+from .cetagory import (
+    ClosetCategorySerializer,
+    ClosetCategoryDetailSerializer,
+    ClosetSubCategorySerializer,
+)
 
 __all__ = [
     'ItemCategorySerializer',
     'ItemCategoryTypeSerializer',
     'ClosetItemSerializer',
+    'ClosetCategorySerializer',
+    'ClosetSubCategorySerializer',
+    'ClosetCategoryDetailSerializer',
 ]
